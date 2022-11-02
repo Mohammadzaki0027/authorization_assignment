5a09fb03b017a73dc46c9f46921dc345dddda689
Mohammadzaki0027 <zaki7875@gmail.com>
2022-11-02 23:57:34 +0530

    auth

--- middleware/Auth.middelware.js
+++ middleware/Auth.middelware.js
@@ -1,3 +1,19 @@
-const Authentication=((req,res,next)=>{
-    
-})
+const { application } = require("express");
+const jwt = require("jsonwebtoken");
+const Authentication = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.send("please login again");
+  }
+  const token = req.headers?.authorization?.split(" ")[1];
+ 
+  jwt.verify(token, "secret", function (err, decoded) {
+    if (err) {
+      res.send("Please Login");
+    } else {
+      req.body.email = decoded.email;
+      next();
+    }
+  });
+};
+
+module.exports={Authentication}
--- route/product.route.js
+++ route/product.route.js
@@ -1,7 +1,13 @@
 const {Router}=require("express")
+const { Authentication } = require("../middleware/Auth.middelware")
 
 const ProductRouter=Router()
-ProductRouter.post("/product",(req,res)=>{
-    res.send("Products")
+ProductRouter.get("/product",Authentication,(req,res)=>{
+    const{email}=req.body
+   if(email){
+    res.send("products...")
+    return
+   }
+    res.send("login again...")
 })
 module.exports={ProductRouter}
